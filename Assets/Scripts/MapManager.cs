770afee1d329c82e988617cb6594ee14da1a5e75
TomasBra <tomik.brabec@gmail.com>
2026-03-05 09:42:03 +0100

    Mining overlay effect

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -9,27 +9,19 @@ public class MapManager : MonoBehaviour
     private Tilemap map;
 
     [SerializeField]
+    private Tilemap overlayMap;
+
+    [SerializeField]
     private TileBase tile;
 
+    [SerializeField]
+    private TileBase overlayTile;
+    [SerializeField]
+    private TileBase overlayTile2;
+    [SerializeField]
+    private TileBase overlayTile3;
+
     private Dictionary<Vector3Int, TileData> tileDatas;
-
-    /*[SerializeField]
-    private List<TileData> tileDatas;
-
-    private Dictionary<TileBase, TileData> dataFromTiles;
-
-    private void Awake()
-    {
-        dataFromTiles = new Dictionary<TileBase, TileData>();
-
-        foreach (var tileData in tileDatas)
-        {
-            foreach (var tile in tileData.tiles)
-            {
-                dataFromTiles.Add(tile, tileData);
-            }   
-        }
-    }*/
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -58,8 +50,23 @@ public class MapManager : MonoBehaviour
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int gridPosition = map.WorldToCell(mousePosition);
 
-            tileDatas[gridPosition].Damage(1);
-            
+            float remainingDuration = tileDatas[gridPosition].Damage(1);
+
+            if (remainingDuration <= 0) {
+                overlayMap.SetTile(gridPosition, null);
+            }
+            else if (remainingDuration < 2)
+            {
+                overlayMap.SetTile(gridPosition, overlayTile3);
+            }else if(remainingDuration < 3)
+            {
+                overlayMap.SetTile(gridPosition, overlayTile2);
+            }else if (remainingDuration < 4)
+            {
+                overlayMap.SetTile(gridPosition, overlayTile);
+            }
+
+
             /*TileBase clickedTile = map.GetTile(gridPosition);
 
             dataFromTiles[clickedTile].durability += -1;
