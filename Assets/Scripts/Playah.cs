3c9cbbe1e4b406669e3c30fed319fb3bbae36ec0
TomasBra <tomik.brabec@gmail.com>
2026-03-05 13:12:12 +0100

    Laser Shader

--- Assets/Scripts/Playah.cs
+++ Assets/Scripts/Playah.cs
@@ -4,11 +4,16 @@ using UnityEngine.Tilemaps;
 public class Playah : MonoBehaviour
 {
     private const float SPEED = 1.69f;
+    private const float LASER_DISTANCE = 7;
+    private const float DAMAGE_PER_SECOND = 5;
 
     new Rigidbody2D rigidbody;
 
     [SerializeField]
     private GameObject MapManager;
+
+    [SerializeField]
+    private GameObject FirePoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,19 +34,38 @@ public class Playah : MonoBehaviour
 
         rigidbody.linearVelocity = direction * SPEED;
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            RayCast2D();
+            Laser2D();
+        }
+        else
+        {
+            GetComponent<LineRenderer>().positionCount = 0;
         }
     }
 
-    void RayCast2D()
+
+    void Laser2D()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = mousePosition - (Vector2)transform.position;
+        direction.Normalize();
+        
         RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
+        MapManager.GetComponent<MapManager>().HitTile(hit.point, DAMAGE_PER_SECOND);
 
-        MapManager.GetComponent<MapManager>().HitTile(hit.point);
+        Vector3 laserEndPosition;
+        if(hit.collider != null)
+            laserEndPosition = new Vector3(hit.point.x, hit.point.y, 0);
+        else
+            laserEndPosition = mousePosition+direction*LASER_DISTANCE;
 
+        DrawLaser(FirePoint.transform.position, laserEndPosition);
+    }
+
+    void DrawLaser(Vector3 startPostion, Vector3 endPosition) {
+        GetComponent<LineRenderer>().positionCount = 2;
+        GetComponent<LineRenderer>().SetPosition(0, startPostion);
+        GetComponent<LineRenderer>().SetPosition(1, endPosition);
     }
 }
