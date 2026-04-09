83ed593cc1714ead2f81f6616e3b28035a4f4c07
Wotan223 <romanhavelka199@seznam.cz>
2026-04-09 14:47:50 +0200

    StatMenu

--- Assets/Scripts/Stats/StatsUI.cs
+++ Assets/Scripts/Stats/StatsUI.cs
@@ -3,18 +3,47 @@ using TMPro;
 public class StatsUI : MonoBehaviour
 {
     public GameObject[] statsSlots;
+    public CanvasGroup statsCanvas;
 
+    private bool statsOpen = false;
     public void Start()
     {
-        Debug.Log(statsSlots.Length);
-        Debug.Log(statsSlots[0].GetComponentInChildren<TMP_Text>().text);
-        if (StatsManager.Instance == null)
-            Debug.Log("Ja jsem se nenacetl..");
+        UpdateAllStats();
+    }
 
-        UpdateDamage();
+    private void Update()
+    {
+        if (Input.GetButtonDown("ToggleStats"))
+        {
+            if (statsOpen)
+            {
+                Time.timeScale = 1;
+                statsCanvas.alpha = 0;
+                statsOpen = false;
+            }
+
+            else
+            {
+                Time.timeScale = 0;
+                statsCanvas.alpha = 1;
+                statsOpen = true;
+            }
+                
+        }
     }
     public void UpdateDamage()
     {
         statsSlots[0].GetComponentInChildren<TMP_Text>().text = "Damage: " + StatsManager.Instance.ENEMY_DAMAGE_PER_SECOND;
+    }
+
+    public void UpdateMiningSpeed()
+    {
+        statsSlots[1].GetComponentInChildren<TMP_Text>().text = "Mining Speed: " + StatsManager.Instance.MINING_DAMAGE_PER_SECOND;
+    }
+
+    public void UpdateAllStats()
+    {
+        UpdateDamage();
+        UpdateMiningSpeed();
     }
 }
