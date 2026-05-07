629ec1fbb8ec3adf3741837ffd00b25bda4e5a2a
Wotan223 <romanhavelka199@seznam.cz>
2026-05-07 10:54:27 +0200

    Menu Opravene
    Opravene Menu juhu

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -1,23 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour
 {
     public GameObject player;
+    public GameObject canvas;
+    public MapManager mapManager;
 
-    void Pause()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (Time.timeScale == 1)
             {
                 Time.timeScale = 0; 
-                player.SetActive(false); 
+                if(mapManager.currentGameState != MapManager.GameState.LANDING)
+                    player.GetComponent<Playah>().enabled = false;
+                canvas.SetActive(true);
+                //player.SetActive(false); 
             }
             else
             {
-                Time.timeScale = 1; 
-                player.SetActive(true); 
+                Time.timeScale = 1;
+                if (mapManager.currentGameState != MapManager.GameState.LANDING)
+                    player.GetComponent<Playah>().enabled = true;
+                canvas.SetActive(false);
+                //player.SetActive(true); 
             }
         }
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        player.GetComponent<Playah>().enabled = true;
+        canvas.SetActive(false);
+        //player.SetActive(true);
+    }
+    public void LeaveGame() 
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync(0); //< nacteni sceny !
     }
 }
